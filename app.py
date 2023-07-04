95b49c2830db2e16dedce042c6b4e884d203fff8
applewjr <james.r.applewhite@gmail.com>
2023-07-04 08:00:12 -0700

    Update app.py

--- app.py
+++ app.py
@@ -696,12 +696,17 @@ def etl_status_dash():
     cursor.execute("""SET time_zone = 'America/Los_Angeles';""")
 
     all_dash = """
-    SELECT 'YouTube' AS table_name, MIN(collected_dt) AS min, MAX(collected_dt) AS max, FORMAT(COUNT(*), 0) AS rows_count FROM youtube_trending
-    UNION ALL SELECT 'Spotify Playlists', MIN(collected_dt), MAX(collected_dt), FORMAT(COUNT(*), 0) FROM spotify_playlists
-    UNION ALL SELECT 'Spotify Artists', MIN(collected_dt), MAX(collected_dt), FORMAT(COUNT(*), 0) FROM spotify_artists
-    UNION ALL SELECT 'Spotify Tracks', MIN(collected_dt), MAX(collected_dt), FORMAT(COUNT(*), 0) FROM spotify_tracks
-    UNION ALL SELECT 'LoL Summoner', MIN(pulled_dt), MAX(pulled_dt), FORMAT(COUNT(*), 0) FROM lol_summoner
-    UNION ALL SELECT 'LoL Champion', MIN(pulled_dt), MAX(pulled_dt), FORMAT(COUNT(*), 0) FROM lol_champion;
+    SELECT 'YouTube' AS table_name, MIN(collected_dt) AS min, MAX(collected_dt) AS max, COUNT(*) AS rows_count FROM youtube_trending
+    UNION ALL SELECT 'Spotify Playlists', MIN(collected_dt), MAX(collected_dt), COUNT(*) FROM spotify_playlists
+    UNION ALL SELECT 'Spotify Artists', MIN(collected_dt), MAX(collected_dt), COUNT(*) FROM spotify_artists
+    UNION ALL SELECT 'Spotify Tracks', MIN(collected_dt), MAX(collected_dt), COUNT(*) FROM spotify_tracks
+    UNION ALL SELECT 'LoL Summoner', MIN(pulled_dt), MAX(pulled_dt), COUNT(*) FROM lol_summoner
+    UNION ALL SELECT 'LoL Champion', MIN(pulled_dt), MAX(pulled_dt), COUNT(*) FROM lol_champion
+    UNION ALL SELECT 'LoL Match',
+        CONVERT_TZ(MIN(gameStartTimestamp), 'UTC', 'America/Los_Angeles') AS min_pst,
+        CONVERT_TZ(MAX(gameStartTimestamp), 'UTC', 'America/Los_Angeles') AS max_pst,
+        COUNT(*)
+    FROM lol_match;
     """
     cursor.execute(all_dash)
     all_dash = cursor.fetchall()
