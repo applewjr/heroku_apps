6039d38c0256347927737b316a07b1659e5fd71b
applewjr <james.r.applewhite@gmail.com>
2024-12-02 16:47:08 -0800

    views update

--- mysql_views/vw_prod_demo.sql
+++ mysql_views/vw_prod_demo.sql
@@ -1,5 +1,9 @@
 CREATE OR REPLACE VIEW vw_prod_demo AS
 
-SELECT * FROM feedback
+SELECT *
+FROM app_visits AS av
+where page_name IN ('https://twitter.com/J_R_Applewhite', 'https://www.linkedin.com/in/j-applewhite/', 'https://github.com/applewjr/heroku_apps')
+	AND referrer NOT LIKE '%5000%'
+ORDER BY submit_time DESC
 LIMIT 25
 ;
