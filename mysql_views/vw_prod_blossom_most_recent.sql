4034c8366e14875352cd42162c1692f0e15d7f39
applewjr <james.r.applewhite@gmail.com>
2024-12-02 16:47:41 -0800

    views update

--- mysql_views/vw_prod_blossom_most_recent.sql
+++ mysql_views/vw_prod_blossom_most_recent.sql
@@ -1,5 +1,23 @@
 CREATE OR REPLACE VIEW vw_prod_blossom_most_recent AS
 
+WITH recent_clicks AS (
+    SELECT *
+    FROM blossom_solver_clicks
+    WHERE click_time <= CONVERT_TZ(NOW(), 'UTC', 'America/Los_Angeles')
+    ORDER BY click_time DESC
+    LIMIT 25
+	)
+,lag_computation AS (
+    SELECT
+	 id
+	,click_time
+    ,TIMESTAMPDIFF(MINUTE, LAG(click_time) OVER (ORDER BY click_time), click_time) AS lag_minute
+    ,must_have
+    ,may_have
+    ,petal_letter
+    ,list_len
+    FROM recent_clicks
+	)
 SELECT
  '-' AS id
 ,CONVERT_TZ(NOW(), 'UTC', 'America/Los_Angeles') AS click_time
@@ -8,17 +26,7 @@ SELECT
 ,'-' AS may_have
 ,'-' AS petal_letter
 ,'-' AS list_len
-FROM blossom_solver_clicks
+FROM recent_clicks
 UNION ALL
-SELECT
- id
-,click_time
-,TIMESTAMPDIFF(MINUTE, LAG(click_time) OVER (ORDER BY click_time), click_time) AS lag_minute
-,must_have
-,may_have
-,petal_letter
-,list_len
-FROM blossom_solver_clicks
-ORDER BY click_time DESC, id DESC
-LIMIT 26
-;
+SELECT * FROM lag_computation
+ORDER BY click_time DESC, id DESC;
